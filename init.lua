7fb454d1d8b891cc59f312f02ff681182ecaa8c3
twistedttea <twistedtteavr@gmail.com>
2024-02-26 13:03:06 -0600

    added remaps, and changed some settings within init lua
    TODO lsp shit

--- init.lua
+++ init.lua
@@ -334,6 +334,36 @@ vim.wo.signcolumn = 'yes'
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
+	vim.opt.guicursor = ""
+	vim.opt.relativenumber = true
+	
+	vim.opt.tabstop = 4
+	vim.opt.softtabstop = 4
+	vim.opt.shiftwidth = 4
+	vim.opt.expandtab = true
+
+	vim.opt.smartindent = true
+
+	vim.opt.wrap = false
+
+	vim.opt.swapfile = false
+	vim.opt.backup = false
+	vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+	vim.opt.undofile = true
+
+	
+	vim.opt.hlsearch = false
+	vim.opt.incsearch = true
+
+	vim.opt.termguicolors = true
+
+	vim.opt.scrolloff = 8
+	vim.opt.signcolumn = "yes"
+	vim.opt.isfname:append("@-@")
+
+	vim.opt.updatetime = 50
+
+	vim.opt.colorcolumn = "80" 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
---
+++ lua/custom/plugins/remap.lua
@@ -0,0 +1,31 @@
+return {
+	-- move blocks of code around when visual selected + auto indent
+	vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv"),
+	vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv"),
+	
+	-- Centering on page jump
+	vim.keymap.set("n", "<C-d>", "<C-d>zz"),
+	vim.keymap.set("n", "<C-u>", "<C-u>zz"),
+	
+	-- centering when clicking 'n' or 'N' to jump to next search
+	vim.keymap.set("n", "n", "nzzzv"),
+	vim.keymap.set("n", "N", "Nzzzv"),
+
+	-- repalce-paste without overwriting yank buffer
+	vim.keymap.set("x", "<leader>p", "\"_dP"),
+
+	-- yank to sys clipboard
+	vim.keymap.set("n", "<leader>y", "\"+y"),
+	vim.keymap.set("v", "<leader>y", "\"+y"),
+	vim.keymap.set("n", "<leader>Y", "\"+Y"),
+	
+	-- quick fix navigation binds
+	vim.keymap.set("n", "<C-k", "<cmd>cnext<CR>zz"),
+	vim.keymap.set("n", "<C-j", "<cmd>cprev<CR>zz"),
+	vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz"),
+	vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz"),
+
+	-- search and replace word under cursor
+	vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>"),
+	vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true }) 
+}
